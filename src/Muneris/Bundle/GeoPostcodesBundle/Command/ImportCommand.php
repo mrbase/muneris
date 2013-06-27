b4058fc4bd2a9d236366e041932508003ccd8d7f
ulrik nielsen <ulrik@o3.dk>
2013-06-27 22:06:41 +0200

    added importer to allow us to import/update geopostcode data

---
+++ src/Muneris/Bundle/GeoPostcodesBundle/Command/ImportCommand.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: un
+ * Date: 27/06/13
+ * Time: 20.27
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Muneris\Bundle\GeoPostcodesBundle\Command;
+
+
+class ImportCommand {
+
+}

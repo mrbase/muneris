998297934a38381f5e26b41a86491f0dbd9390b8
ulrik nielsen <ulrik@o3.dk>
2014-01-24 11:40:34 +0100

    remapped new data file format from geopostalcode.com

--- src/Muneris/Bundle/GeoPostcodesBundle/Command/GpcImportCommand.php
+++ src/Muneris/Bundle/GeoPostcodesBundle/Command/GpcImportCommand.php
@@ -43,14 +43,22 @@ class GpcImportCommand extends ContainerAwareCommand
 
         $conn = $this->getContainer()->get('database_connection');
 
+        $output->writeln('Truncating GeoPostcode to make room for the new data set.');
+        $conn->exec("TRUNCATE TABLE GeoPostcode");
+        $output->writeln('<info>- Done</info>');
+
         $progress = $this->getHelperSet()->get('progress');
         $progress->setRedrawFrequency(1000);
         $progress->setBarWidth(100);
 
-        $output->writeln('<info>Starting import, this might take a while..</info>');
+        $output->writeln('Starting import, this might take a while..');
+
+        $headers = ['iso', 'country', 'language', 'id', 'region1', 'region2', 'region3', 'region4', 'locality', 'postcode', 'suburb', 'street', 'range', 'latitude', 'longitude', 'elevation', 'iso2', 'fips', 'nuts', 'hasc', 'stat', 'timezone', 'utc', 'dst'];
 
         $start = time();
         $i = -1;
+        $sequence = 0;
+        $country = '';
         while (($data = fgetcsv($fp, 2048, ';')) !== false) {
             if (24 !== count($data)) {
                 $output->writeln('<error>Wrong column count for data file.</error>');
@@ -64,32 +72,41 @@ class GpcImportCommand extends ContainerAwareCommand
                 continue;
             }
 
-            iso;country;language;id;region1;region2;region3;region4;locality;postcode;suburb;street;range;latitude;longitude;elevation;iso2;fips;nuts;hasc;stat;timezone;utc;dst
+            // remap using headers, easier to read..
+            $mapped = [];
+            foreach ($data as $k => $v) {
+                $mapped[$headers[$k]] = trim($v);
+            }
 
+            if ($mapped['iso'] != $country) {
+                $country = $mapped['iso'];
+                $sequence = 0;
+            }
 
             $conn->insert('GeoPostcode', [
-                'country'     => $data[1],
-                'language'    => $data[2],
-                'sequence'    => $data[3],
-                'region_code' => $data[4],
-                'region_1'    => $data[5],
-                'region_2'    => $data[6],
-                'region_3'    => $data[7],
-                'region_4'    => $data[8],
-                'zip_code'    => $data[9],
-                'city'        => $data[10],
-                'area_1'      => $data[11],
-                'area_2'      => $data[12],
-                'lat'         => $data[13],
-                'lng'         => $data[14],
-                'tz'          => $data[15],
-                'utc'         => $data[16],
-                'dst'         => $data[17],
+                'country'     => $mapped['iso'],
+                'language'    => $mapped['language'],
+                'sequence'    => $sequence,
+                'region_code' => $mapped['id'],
+                'region_1'    => $mapped['region1'],
+                'region_2'    => $mapped['region2'],
+                'region_3'    => $mapped['region3'],
+                'region_4'    => $mapped['region4'],
+                'zip_code'    => $mapped['postcode'],
+                'city'        => $mapped['locality'],
+                'area_1'      => $mapped['suburb'],
+                'area_2'      => $mapped['street'],
+                'lat'         => $mapped['latitude'],
+                'lng'         => $mapped['longitude'],
+                'tz'          => $mapped['timezone'],
+                'utc'         => $mapped['utc'],
+                'dst'         => $mapped['dst'],
                 'created_at'  => 'now()',
                 'updated_at'  => 'now()',
             ]);
 
-            $progress->advance(strlen(implode('', $data))+45);
+            $sequence++;
+            $progress->advance(strlen(implode('', $data))+71);
         }
         $progress->finish();
         $output->writeln('<info>Done! Imported '.$i.' records in '.(time() - $start).' seconds.</info>');

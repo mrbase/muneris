7579a036e1ca42442a2c876da7e2fa4c4c82d778
ulrik nielsen <ulrik@o3.dk>
2014-03-20 23:39:28 +0100

    thin controllers - everything moved to services

--- src/Muneris/Bundle/GeoPostcodesBundle/Controller/PostcodesController.php
+++ src/Muneris/Bundle/GeoPostcodesBundle/Controller/PostcodesController.php
@@ -2,48 +2,50 @@
 
 namespace Muneris\Bundle\GeoPostcodesBundle\Controller;
 
-use Muneris\Bundle\GeoPostcodesBundle\Entity\GeoPostcode;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Util\Codes;
+use Muneris\Bundle\GeoPostcodesBundle\Entity\LocalGeoPostcode;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use FOS\RestBundle\Controller\Annotations\View;
+use FOS\RestBundle\View\View AS FOSView;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Sensio\Bundle\FrameworkExtraBundle\EventListener\ParamConverterListener;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\Stopwatch\Stopwatch;
 
-class PostcodesController extends Controller
+class PostcodesController extends FOSRestController
 {
     /**
      * @param  string $country
      * @return array
      * @throws NotFoundHttpException
      * @View()
+     *
+     * @ApiDoc(
+     *   resource     = true,
+     *   description  = "Get all zip codes in a given country.",
+     *   output       = "Muneris\Bundle\GeoPostcodesBundle\Entity\GeoPostcode",
+     *   requirements = {
+     *     {"name"="country", "description"="Iso2 country code.", "dataType"="string"}
+     *   },
+     *   statusCodes = {
+     *     200 = "Returned when successful",
+     *     404 = "Returned when the ressource is not found."
+     *   }
+     * )
+     *
      * @Cache(smaxage="86400")
      */
     public function getCountryPostcodesAction($country)
     {
-        $stopwatch = new Stopwatch();
-        $stopwatch->start('lookup');
-
-        $postcodes = $this->getDoctrine()
-            ->getRepository('MunerisGeoPostcodesBundle:GeoPostcode')
-            ->findCities([':country' => $country])
+        return $this
+            ->container
+            ->get('muneris_geo_postcodes.postcode.handler')
+            ->countrySearch($country)
         ;
-
-        if (0 == count($postcodes)) {
-            throw new NotFoundHttpException("No zip codes available for '".$country."'");
-        }
-
-        $response = [
-            'postcodes' => $postcodes,
-            '_time'     => $stopwatch->stop('lookup')->getDuration().'ms',
-        ];
-
-        $jsonp = $this->get('muneris.jsonp.handler');
-        if ($jsonp->isJsonpRequest()) {
-            return $jsonp->sendResponse($response);
-        }
-
-        return $response;
     }
+
 
     /**
      * @param  string $country
@@ -51,34 +53,32 @@ class PostcodesController extends Controller
      * @return array
      * @throws NotFoundHttpException
      * @Cache(smaxage="86400")
+     *
+     * @ApiDoc(
+     *   resource     = true,
+     *   description  = "Search for zip code in a given country.",
+     *   output       = "Muneris\Bundle\GeoPostcodesBundle\Entity\GeoPostcode",
+     *   requirements = {
+     *     {"name"="country", "description"="Iso2 country code.", "dataType"="string"},
+     *     {"name"="zip_code", "description"="Zip code to lookup", "dataType"="string"}
+     *   },
+     *   statusCodes = {
+     *     200 = "Returned when successful",
+     *     404 = "Returned when the ressource is not found."
+     *   }
+     * )
+     *
      * @View()
      */
     public function getCountryPostcodeAction($country, $zip_code)
     {
-        $stopwatch = new Stopwatch();
-        $stopwatch->start('lookup');
-
-        $postcodes = $this->getDoctrine()
-            ->getRepository('MunerisGeoPostcodesBundle:GeoPostcode')
-            ->findByFuzzy($country, $zip_code)
+        return $this
+            ->container
+            ->get('muneris_geo_postcodes.postcode.handler')
+            ->fuzzySearch($country, $zip_code)
         ;
-
-        if (0 == count($postcodes)) {
-            throw new NotFoundHttpException('Zip code not found');
-        }
-
-        $response = [
-            'postcodes' => $postcodes,
-            '_time'    => $stopwatch->stop('lookup')->getDuration().'ms',
-        ];
-
-        $jsonp = $this->get('muneris.jsonp.handler');
-        if ($jsonp->isJsonpRequest()) {
-            return $jsonp->sendResponse($response);
-        }
-
-        return $response;
     }
+
 
     /**
      * @param  string $city
@@ -86,35 +86,30 @@ class PostcodesController extends Controller
      * @return array
      * @throws NotFoundHttpException
      * @Cache(smaxage="86400")
+     *
+     * @ApiDoc(
+     *   resource     = true,
+     *   description  = "Search for city names in a given country.",
+     *   output       = "Muneris\Bundle\GeoPostcodesBundle\Entity\GeoPostcode",
+     *   requirements = {
+     *     {"name"="country", "description"="Iso2 country code.", "dataType"="string"},
+     *     {"name"="city", "description"="City name to lookup", "dataType"="string"}
+     *   },
+     *   statusCodes = {
+     *     200 = "Returned when successful",
+     *     404 = "Returned when the ressource is not found."
+     *   }
+     * )
+     *
      * @View()
      */
     public function getCountryCityAction($country, $city)
     {
-        $stopwatch = new Stopwatch();
-        $stopwatch->start('lookup');
-
-        $postcodes = $this->getDoctrine()
-            ->getRepository('MunerisGeoPostcodesBundle:GeoPostcode')
-            ->findBy([
-                'city'    => urldecode($city),
-                'country' => $country
-        ]);
-
-        if (0 == count($postcodes)) {
-            throw new NotFoundHttpException('City code not found');
-        }
-
-        $response = [
-            'postcodes' => $postcodes,
-            '_time'    => $stopwatch->stop('lookup')->getDuration().'ms',
-        ];
-
-        $jsonp = $this->get('muneris.jsonp.handler');
-        if ($jsonp->isJsonpRequest()) {
-            return $jsonp->sendResponse($response);
-        }
-
-        return $response;
+        return $this
+            ->container
+            ->get('muneris_geo_postcodes.postcode.handler')
+            ->citySearch($country, $city)
+        ;
     }
 
     /**
@@ -123,32 +118,130 @@ class PostcodesController extends Controller
      * @return array
      * @throws NotFoundHttpException
      * @Cache(smaxage="86400")
+     *
+     * @ApiDoc(
+     *   resource     = true,
+     *   description  = "Do funkey search for postal codes for a given country.",
+     *   output       = "Muneris\Bundle\GeoPostcodesBundle\Entity\GeoPostcode",
+     *   requirements = {
+     *     {"name"="country", "description"="Iso2 country code.", "dataType"="string"},
+     *     {"name"="fuzzy", "description"="Postal code and/or city name to lookup", "dataType"="string"}
+     *   },
+     *   statusCodes = {
+     *     200 = "Returned when successful",
+     *     404 = "Returned when the ressource is not found."
+     *   }
+     * )
+     *
      * @View()
      */
     public function getCountryFuzyAction($country, $fuzzy)
     {
-        $stopwatch = new Stopwatch();
-        $stopwatch->start('lookup');
-
-        $postcodes = $this->getDoctrine()
-            ->getRepository('MunerisGeoPostcodesBundle:GeoPostcode')
-            ->findByFuzzy($country, $fuzzy)
+        return $this
+            ->container
+            ->get('muneris_geo_postcodes.postcode.handler')
+            ->fuzzySearch($country, $fuzzy)
         ;
+    }
 
-        if (0 == count($postcodes)) {
-            throw new NotFoundHttpException('City code not found');
+
+    /**
+     * @return string
+     * @ApiDoc(
+     *   resource    = true,
+     *   description = "Creates a new page from the submitted data.",
+     *   input       = "Muneris\Bundle\GeoPostcodesBundle\Form\LocalGeoPostcodeType",
+     *   statusCodes = {
+     *     200 = "Returned when successful",
+     *     400 = "Returned when the form has errors"
+     *   }
+     * )
+     *
+     * @View(
+     *  template    = "MunerisGeoPostcodesBundle:Postcodes:new.html.twig",
+     *  statusCode  = Codes::HTTP_BAD_REQUEST,
+     *  templateVar = "form"
+     * )
+     */
+    public function postPostcodeAction()
+    {
+        return $this->processForm(new LocalGeoPostcode());
+    }
+
+
+    /**
+     * Update existing page from the submitted data or create a new page at a specific location.
+     *
+     * @ApiDoc(
+     *   resource    = true,
+     *   description = "Updating an existing postcode.",
+     *   input       = "Muneris\Bundle\GeoPostcodesBundle\Form\LocalGeoPostcodeType",
+     *   statusCodes = {
+     *     204 = "Returned when successful",
+     *     400 = "Returned when the form has errors",
+     *     404 = "Returned if the postal code is not found in the local namespace."
+     *   }
+     * )
+     *
+     * @View(
+     *  template    = "MunerisGeoPostcodesBundle:Postcodes:new.html.twig",
+     *  templateVar = "form"
+     * )
+     *
+     * @param int     $id      the page id
+     *
+     * @return FormTypeInterface|View
+     *
+     * @throws NotFoundHttpException when page not exist
+     */
+    public function putPostcodeAction($id)
+    {
+        if (null === $postcode = $this->container->get('muneris_geo_postcodes.postcode.handler')->findLocalById($id)) {
+            throw new NotFoundHttpException('No such postcode.');
         }
 
-        $response = [
-            'postcodes' => $postcodes,
-            '_time'    => $stopwatch->stop('lookup')->getDuration().'ms',
-        ];
+        return $this->processForm($postcode);
+    }
 
-        $jsonp = $this->get('muneris.jsonp.handler');
-        if ($jsonp->isJsonpRequest()) {
-            return $jsonp->sendResponse($response);
-        }
 
-        return $response;
+    /**
+     * Form processing for working with the Local copies of postcodes.
+     *
+     * @param  LocalGeoPostcode $postcode
+     * @return \FOS\RestBundle\View\View|int
+     */
+    private function processForm(LocalGeoPostcode $postcode)
+    {
+        $request = $this->getRequest();
+
+        return $this
+            ->container
+            ->get('muneris_geo_postcodes.postcode.handler')
+            ->processLocal(
+                $postcode,
+                $request->request->all(),
+                $request->getMethod(),
+                $request->get('_format')
+        );
+    }
+
+
+    /**
+     * Presents the form to use to create a new page.
+     *
+     * @ApiDoc(
+     *   resource    = true,
+     *   statusCodes = {
+     *     200 = "Returned when successful"
+     *   }
+     * )
+     *
+     * @View()
+     *
+     * @return FormTypeInterface
+     */
+    public function newPostcodeAction()
+    {
+        return $this->createForm('localpostcode', new LocalGeoPostcode());
     }
 }
